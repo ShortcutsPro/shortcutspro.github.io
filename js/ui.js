0a7bb44b2884111f1a60cb870b604dab072e12eb
Scott Wright <scott@wright.name>
2021-07-06 07:20:57 -0700

    q

--- js/ui.js
+++ js/ui.js
@@ -1,12 +1,12 @@
-const recipes = document.querySelector(‘.recipes’);
+const recipes = document.querySelector'.recipes');
 
-document.addEventListener(‘DOMContentLoaded’, function() {
+document.addEventListener('DOMContentLoaded', function() {
   // nav menu
-  const menus = document.querySelectorAll(‘.side-menu’);
-  M.Sidenav.init(menus, {edge: ‘right’});
+  const menus = document.querySelectorAll('.side-menu');
+  M.Sidenav.init(menus, {edge: 'right’});
   // add recipe form
-  const forms = document.querySelectorAll(‘.side-form’);
-  M.Sidenav.init(forms, {edge: ‘left’});
+  const forms = document.querySelectorAll('.side-form’);
+  M.Sidenav.init(forms, {edge: 'left’});
 });
 
 // render recipe data
