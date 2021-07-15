614190c8bb1a6984d3bbfd3a0f8559c2815f92e7
Scott Wright <scott@wright.name>
2021-07-15 00:58:08 -0700

    localStorage working

--- js/db.js
+++ js/db.js
@@ -1,33 +1,31 @@
-
-
 // add new shortcut
 const form = document.querySelector('form');
 form.addEventListener('submit', e => {
   e.preventDefault();
-  
+
   const shortcut = {
     name: form.name.value,
     input: form.input.value,
     icon: form.icon.value
   };
-  
-  console.log(shortcut.name);
-  
-  localStorage.setItem('key', 'value');
-      
-  
+
+  if (shortcut.name.length) {
+      localStorage.setItem(form.name.value, JSON.stringify(shortcut));
+  };
+
   form.name.value = '';
   form.input.value = '';
   form.icon.value = '';
+  
+  location.reload();
 });
 
 
 // remove a shortcut
 const shortcutContainer = document.querySelector('.shortcuts');
-shortcutContainer.addEventListener('click', evt => {
-  if(evt.target.tagName === 'I'){
-    const id = evt.target.getAttribute('name');
-    //console.log(id);
-    localStorage.setItem(id, null);
-  }
-})
+shortcutContainer.addEventListener('click', e => {
+    const id = e.target.getAttribute('data-id');
+    console.log('clicked');
+    localStorage.removeItem(id);
+    location.reload();
+});
--- js/ui.js
+++ js/ui.js
@@ -16,14 +16,14 @@ for (var i = 0; i < localStorage.length; i++) {
   let data = JSON.parse(localStorage.getItem(localStorage.key(i)));
   
   let dict = encodeURIComponent(`{\"name\":\"${data.name}\",\"input\":\"${data.input}\"}`);
-  console.log(`${data.name}`);
+  // console.log(`${data.name}`);
   
   const html = `
     <div class="card-panel shortcut white row" data-id="${data.name}">
             <img class="img" src="${data.icon}" alt="shortcut icon" />
       <a href="shortcuts://run-shortcut?name=INTEGRITY&input=text&text=${dict}">
         <div class="shortcut-details">
-          <div class="shortcut-title">${data.name}</div>
+          <div class="shortcut-name">${data.name}</div>
           <div class="shortcut-input">${data.input}</div>
         </div>
       </a>
@@ -38,7 +38,8 @@ for (var i = 0; i < localStorage.length; i++) {
 };
 
 // remove shortcut
-const removeShortcut = (i) => {
-  const shortcut = document.querySelector(`.shortcut[data-id=${i}]`);
+const removeShortcut = (id) => {
+  const shortcut = document.querySelector(`.shortcut[data-id=${id}]`);
+  console.log('logged');
   shortcut.remove();
 };
